015b8e52312ebd263c4b103f259830b7d983f1a1
Abdul Wasay <93523515+WasayChohan@users.noreply.github.com>
2023-08-18 14:34:40 -0700

    third commit

--- src/Home.js
+++ src/Home.js
@@ -15,27 +15,27 @@ function Home() {
         <div className="home_row">
           <Product
             id="12321341"
-            title="The Lean Startup: How Constant Innovation Creates Radically Successful Businesses Paperback"
+            title="Step into a world of endless possibilities with the iPhone X in black, where cutting-edge meets timeless elegance."
             price={11.96}
             rating={5}
-            image="https://images-na.ssl-images-amazon.com/images/I/51Zymoq7UnL._SX325_BO1,204,203,200_.jpg"
+            image="https://media.very.ie/i/littlewoodsireland/VBZ9D_SQ1_0000000674_SPACE_GREY_SLf/premium-pre-loved-grade-a-iphone-xs-64gb-space-grey-ntn.jpg?$180x240_retinamobilex2$"
           />
           <Product
             id="49538094"
-            title="Kenwood kMix Stand Mixer for Baking, Stylish Kitchen Mixer with K-beater, Dough Hook and Whisk, 5 Litre Glass Bowl"
+            title="Make a statement with our eye-catching scooty, a bold expression of your individuality and zest for life"
             price={239.0}
             rating={4}
-            image="https://images-na.ssl-images-amazon.com/images/I/81O%2BGNdkzKL._AC_SX450_.jpg"
+            image="https://sxcscooters.com/cdn/shop/products/arch1_1aa1d799-3a54-4a6b-9c22-937afc4f868e_1000x.jpg?v=1679059479"
           />
         </div>
 
         <div className="home_row">
           <Product
             id="4903850"
-            title="Samsung LC49RG90SSUXEN 49' Curved LED Gaming Monitor"
+            title="Walk the path of sophistication with the Black Swan handbag, a tribute to grace and poise"
             price={199.99}
             rating={3}
-            image="https://images-na.ssl-images-amazon.com/images/I/71Swqqe7XAL._AC_SX466_.jpg"
+            image="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSZj4r8-GcVbJflpKJu-CU-Dc5HXSnyAU8tz9oH1MJg1WAJRbQ8p4NY0b27cy9KNj0eDic&usqp=CAU"
           />
           <Product
             id="23445930"
@@ -56,10 +56,10 @@ function Home() {
         <div className="home_row">
           <Product
             id="90829332"
-            title="Samsung LC49RG90SSUXEN 49' Curved LED Gaming Monitor - Super Ultra Wide Dual WQHD 5120 x 1440"
+            title="Experience the touch of luxury with our rejuvenating cream, where indulgence meets skincare"
             price={1094.98}
             rating={4}
-            image="https://images-na.ssl-images-amazon.com/images/I/6125mFrzr6L._AC_SX355_.jpg"
+            image="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQBrJWDGf4t0F8_o-3iNwASSL2yuSKyEspwYQ&usqp=CAU"
           />
         </div>
       </div>
